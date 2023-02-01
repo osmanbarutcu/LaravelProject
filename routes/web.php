929e9791027a61bec92dd52173585d71a40e1464
osmanbarutcu <ozmanbarutcu@gmail.com>
2023-02-01 13:56:45 +0300

    güncelle

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Ornek;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/deneme',function(){
+// return view('ornek');
+// });
+
+
+
+Route::get('/osman/{isim}',[Ornek::class,'test']); 
